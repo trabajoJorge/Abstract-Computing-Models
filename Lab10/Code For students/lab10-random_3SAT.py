c9f435ad1e8012c7dc5274e75af761dfc0cc779d
Jorge Iglesias <trabajo.jorge2000@gmail.com>
2021-11-19 21:40:17 +0100

    Lab 10 - Finalizado

--- Lab10/Code For students/lab10-random_3SAT.py
+++ Lab10/Code For students/lab10-random_3SAT.py
@@ -1,8 +1,6 @@
 import math
 from random import choice
 from random import randrange
-from lab07_pre_processing import sat_preprocessing
-from Lab09_sat_solver import solve_SAT
 
 
 def procesado_Clausula(clause, assigment):
@@ -28,35 +26,37 @@ def isSatisfactible(clausulas, asig):
             return False
     return True
 
-def sigLit(lastElection):
+def sigLit(lastElection, clause):
     if lastElection==-1: return randrange(3)
-    elif lastElection==2: return randrange(2)
-    elif lastElection==1: 
-        r=randrange(2)
-        if r==1: return 2
-        else: return 0
-    else: #lastElection==0
-        r=randrange(2)
-        if r==1: return 1
-        else: return 2
+    else:
+        while True: 
+            rand= randrange(3)
+            if (lastElection!=clause[randrange(3)]): return rand
 
 def random_3SAT (clauses, num_variables):
     print("_____________________________________________________________________________________")
     vueltas= 10/((2/3)**num_variables)
     vueltas= math.ceil(vueltas) #Para truncar
     print("Vueltas: ", vueltas)
-    assignment = [None] + [choice([0,1]) for n in range(num_variables)]
     lastElecction=-1
 
-    for i in range(vueltas):
-        for j in range(1, num_variables+1):
-            if not procesado_Clausula(clauses[i], assignment): 
-                lastElecction= sigLit(lastElecction)
-                if assignment[abs(clauses[i][lastElecction])]==0: assignment[abs(clauses[i][lastElecction])]=1
-                else: assignment[abs(clauses[i][lastElecction])]=0
-            if isSatisfactible(clauses, assignment): return "SAT"
+    mas=vueltas
+    while vueltas:
+        assignment = [0] + [choice([0,1]) for n in range(num_variables)]
+        for j in range(len(clauses)):
+            if not procesado_Clausula(clauses[j], assignment): 
+                lastElecction= sigLit(lastElecction, clauses[j])
+                lastElecction= abs(clauses[j][lastElecction])
+                if assignment[lastElecction]==0: assignment[lastElecction]=1
+                else: assignment[lastElecction]=0
+            if isSatisfactible(clauses, assignment): 
+                assert isSatisfactible(clauses, assignment)
+                print ("Fin en vuelta ", mas-vueltas)
+                return "SAT"
+        vueltas= vueltas-1
     
     assert not isSatisfactible(clauses, assignment);
+    print ("Fin en vuelta ", mas-vueltas)
     return "UNSAT"
 
 
@@ -71,6 +71,7 @@ def test():
     if r=="SAT": aciertos+=1
     else: fallos+=1
     
+
     clauses = [ [3, 1, 2], [1, 2, -3],[-3, 1, -2], [1, 3, -2],
                 [3, -1, 2], [-1, 2, -3],[-3, -1, -2], [-1, 4, -2]]
     r=random_3SAT(clauses, 4) 
@@ -103,7 +104,7 @@ def test():
     if r=="UNSAT": aciertos+=1
     else: fallos+=1
 
-    print("Esta ejecucion ha fallado: ", fallos, " veces.")
+    print("\n\nEsta ejecucion ha fallado: ", fallos, " veces.")
     print("Esta ejecucion ha acertado: ", aciertos, " veces.")
 
 test()    
